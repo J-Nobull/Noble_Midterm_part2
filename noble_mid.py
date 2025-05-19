29c2877ef11b38eb7e7fad6f6aab0d39595cbb5b
Jason <jason.leo.noble@gmail.com>
2025-05-18 23:41:38 -0700

    Update noble_mid.py

--- noble_mid.py
+++ noble_mid.py
@@ -18,7 +18,7 @@ y = df['race/ethnicity']
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.25, random_state=42)
-model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+model = LogisticRegression(solver='lbfgs', max_iter=1000)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
